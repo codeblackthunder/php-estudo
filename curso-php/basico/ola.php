6f6b3ba81d16d6919c14e6cf5972f508948b2d0c
Felipe <felipe.machado.silva.05@gmail.com>
2019-07-18 10:52:49 -0300

    Tag PHP e comando echo
    Trabalhamos com duas tags com PHP nesta aula :
    <?php ?> e <?= =?> ,sendo que a ultima é usada para mostrar dados e efetuar operações.Comando echo utilizado para escrever no HTML

---
+++ curso-php/basico/ola.php
@@ -0,0 +1,17 @@
+<div class="titulo">Ola PHP</div>
+<h2>Exemplo A</h2>
+<?php 
+//imprimir HTML
+//deve usar ; no fim de cada sentença
+    echo 'Olá <br>'; //podemos colocar tag html dentro do echo como o br que da quebra de linha
+    //não precisa colocar; na ultima sentença
+    echo 'Mundo!'
+
+    
+    ?>
+<h2>Exemplo B</h2>
+    <?="Outra forma de me expressar" ?><!-- bloco utilizaado para impressão sem a necessidade de usar echo--> 
+
+   <?php
+
+   phpinfo(); // saber informações de configurações

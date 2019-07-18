7fdf0fd20927d8bfab4b68f0a3bd19939c6c92f7
Felipe <felipe.machado.silva.05@gmail.com>
2019-07-18 11:35:07 -0300

    Integração com CSS
    Nesta aula foi mostrado diversas formas de interagir PHP com HTML.

--- curso-php/index.php
+++ curso-php/index.php
@@ -19,6 +19,7 @@
                     <ul>
                         <li><a href="exercicio.php?dir=basico&file=ola">Olá php</a></li>
                         <li><a href="exercicio.php?dir=basico&file=html">Integração HTML</a></li>
+                        <li><a href="exercicio.php?dir=basico&file=css">Integração CSS</a></li>
 
                     </ul>
                 </div>
